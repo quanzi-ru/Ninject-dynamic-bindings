9f74dbaa58469cdbc1a786d4ce6d4249735ab547
Konstantin Peshekhonov <konstantin.peshekhonov@sidenis.com>
2016-04-20 12:09:20 +0300

    NullRef fixed

--- NInjectFactoryTest/DynamicBinding.Core/CustomKernel.cs
+++ NInjectFactoryTest/DynamicBinding.Core/CustomKernel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using Ninject;
 using Ninject.Activation;
+using Ninject.Activation.Providers;
 using Ninject.Parameters;
+using Ninject.Planning;
 using Ninject.Planning.Bindings;
+using Ninject.Planning.Targets;
+using Ninject.Selection;
+using Ninject.Selection.Heuristics;
 
 namespace DynamicBinding.Core
 {
@@ -25,10 +32,10 @@ namespace DynamicBinding.Core
 
                 var parameter = parameters.First();
                 var customRequest = new Request(service, (m) => true, new IParameter[0], () => null, false, false);
-
+                
                 var values = parameter.GetValue(CreateContext(customRequest, binding), null) as IEnumerable<string>;
 
-                existingBindings.AddRange(values.Select(value => CreateCustomBinding(service, value)));
+                existingBindings.AddRange(values.Select(value => CreateCustomBinding(service, binding.ProviderCallback, value)));
             }
 
             existingBindings.RemoveAll(b => b.Parameters.Any(p => p is DynamicBindingParameter));
@@ -36,10 +43,11 @@ namespace DynamicBinding.Core
             return existingBindings;
         }
 
-        private IBinding CreateCustomBinding(Type service, string value)
+        private IBinding CreateCustomBinding(Type service, Func<IContext, IProvider> providerCallback, string value)
         {
             var binding = new Binding(service);
             binding.Parameters.Add(new ConstructorArgument("input", value));
+            binding.ProviderCallback = providerCallback;
             return binding;
         }
     }
